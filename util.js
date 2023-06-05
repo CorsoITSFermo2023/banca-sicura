1111a858c9638d79e8235bf34812d304b988f5a9
Lorenzo <iachini.lorenzo@itssmartacademy.it>
2023-06-05 17:46:41 +0200

    token

--- util.js
+++ util.js
@@ -1,9 +1,18 @@
-const { createHash } = require('crypto')
+const { createHash, randomBytes } = require('crypto')
 
 function md5(string) {
   return createHash('md5').update(string).digest("hex")
 }
 
+// generazione token
+function get_random(){
+  const Promise = new Promise (resolve,reject)
+  randomBytes(48, function(ex, buf) {
+  token = buf.toString('base64').replace(/\//g,'_').replace(/\+/g,'-');
+});
+  
+}
+
 module.exports = {
-  md5
+  md5,get_random
 }
