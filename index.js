f171eb4bc3407871090cea64c07c5d7238100246
Ivan Saccutelli <ivansaccutelli@gmail.com>
2023-06-05 17:25:18 +0200

    token

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const initStruct = require("./init-struct");
 
+
+// generazione token
+require('crypto').randomBytes(48, function(ex, buf) {
+    token = buf.toString('base64').replace(/\//g,'_').replace(/\+/g,'-');
+});
+
+
 initStruct();
