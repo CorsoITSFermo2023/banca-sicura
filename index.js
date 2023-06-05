f861e5f8134f76f25d873cbe201e6f3885d0a020
Lorenzo <iachini.lorenzo@itssmartacademy.it>
2023-06-05 17:45:47 +0200

    funzione token

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
 const initStruct = require("./init-struct");
 
 
-// generazione token
-require('crypto').randomBytes(48, function(ex, buf) {
-    token = buf.toString('base64').replace(/\//g,'_').replace(/\+/g,'-');
-});
 
-
-initStruct();
